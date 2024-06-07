15a9474a0b11b7e5e9fb8ddee35041ad0bbb424c
Mario Rojas <135486752+MarioDevelop3r@users.noreply.github.com>
2024-06-06 21:43:42 -0400

    feat: Add function to fetch individual character details

--- src/functions/funciones.jsx
+++ src/functions/funciones.jsx
@@ -4,7 +4,13 @@ const todosPersonajes = async (state) => {
     const peticion = await axios.get('https://rickandmortyapi.com/api/character')
     state(peticion.data.results)
 
+
+    const unicoPersonaje = async (id, state) => {
+        const peticion = await axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+        console.log(peticion)
+    }
+
 }
 export {
-    todosPersonajes
+    
 }
